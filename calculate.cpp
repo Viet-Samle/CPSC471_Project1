8946fe5ac4b9498b624c8126bcac44789617e9c1
Viet-Samle <viet.sam.n.le@gmail.com>
2018-10-10 12:16:49 -0700

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,77 +1,112 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 #define N 3
 #define M 4
 
+class event {
+  public:
+    string e;
+    event();
+};
 
 class process {
   public:
-    string *events;
+    event *p_events;
     int all_events;
     int events_done;
+    int LC_value;
     bool done;
     process();
 };
 process::process(void) {
-  events = new string[M];
+  p_events = new event[M];
+  LC_value = 0;
   all_events = 0;
-  events_done = 0;
+  events_done = 0; // also works as an index on what event is not done
   done = false;
 }
 
-void process_print(process *p) {
-  for (int i = 0; i < p->all_events; i++) {
-    cout << p->events[i] << " ";
-  }
-  cout << endl;
-}
-
 int main() {
-  int last_sent = 1;
+  int last_sent = 0;
   string input;
   process all_processes[N];
   bool all_done = false;
-  process *p0;
-  process *p1;
-  process *p2;
+  int current_p = 0;
+  int send_arg;
+  int receive_arg;
 
   cout << "Enter the input for the logical clock" << endl;
   cout << "Enter the events for the first process" << endl;
   for (int i = 0; i < M; i++) {
     cin >> input;
-    cout << "seg fault 1" << endl;
     if (input != "NULL") {
-      cout << input << endl;
-      p0->events[i] = input;
-      cout << "seg fault 3" << endl;
-      p0->all_events++;
+      all_processes[0].p_events[i].e = input;
+      //cout << all_processes[0].events[i][0] << endl;
+      all_processes[0].all_events++;
     }
+
   }
+
   cout << "Enter the events for the second process" << endl;
   for (int i = 0; i < M; i++) {
     cin >> input;
     if (input != "NULL") {
-      p1->events[i] = input;
-      p1->all_events++;
+      all_processes[1].p_events[i].e = input;
+      all_processes[1].all_events++;
     }
+
   }
   cout << "Enter the events for the third process" << endl;
   for (int i = 0; i < M; i++) {
     cin >> input;
     if (input != "NULL") {
-      p2->events[i] = input;
-      p2->all_events++;
+      all_processes[2].p_events[i].e = input;
+      all_processes[2].all_events++;
     }
+
   }
 
-  /*
+
   while (!all_done) {
+    if (all_processes[current_p].done == false) {
+
+      
+      if (all_processes[current_p].p_events[all_processes[current_p].events_done].e[0] == 's') {
+        send_arg = atoi((char *)all_processes[current_p].p_events[all_processes[current_p].events_done].e[1]);
+        if (send_arg == last_sent + 1) {
+           // TODO : Assign the LC value
+        }
+      }
+      else if (all_processes[current_p].p_events[all_processes[current_p].events_done].e[0] == 'r') {
+        receive_arg = atoi((char *)all_processes[current_p].p_events[all_processes[current_p].events_done].e[1]);
+        if (receive_arg == last_sent) {
+          // TODO: Assign the LC value
+        }
+        else {
+
+        }
+      }
+      // the event is internal and will be assigned an LC value
+      else {
+        // TODO: Assign the LC Value
+      }
+
+
+      if (all_processes[current_p].events_done == M) all_processes[current_p].done = true;
+      current_p = current_p++ % 3;
+    }
+    else {
+      current_p = current_p++ % 3;
+    }
+
+    if (all_processes[0].done == true && all_processes[1].done == true && all_processes[2].done == true) all_done = true;
 
   }
-  */
+
 
 
 
