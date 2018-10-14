917a96e7574e326a9b345b91aaf2ae3bbdba10f9
Viet-Samle <viet.sam.n.le@gmail.com>
2018-10-14 15:28:25 -0700

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include <sstream>
 #include <cstdlib>
 #include <algorithm>
 
@@ -32,8 +31,8 @@ public:
 process::process(void) {
     p_events = new event[M];
     current_LCvalue = 0;
-    all_events = 0; //Number of events in the process
-    events_done = 0; // also works as an index on what event is not done
+    all_events = 0; // Number of events in the process
+    events_done = 0; // Works as a counter for what events are done in each process
     done = false;
 }
 
@@ -42,66 +41,51 @@ int main() {
     process all_processes[N];
     bool all_done = false;
     int current_p = 0;
+    int current_event;
     int send_arg;
-    int event_count;
     int receive_arg;
     int sendarray[10] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
     char* char_temp;
 
     cout << "Enter the input for the logical clock" << endl;
-    cout << "Enter the events for the first process" << endl;
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < N; i++) {
+      cout << "Enter the events for process " << i << endl;
+      for (int j = 0; j < M; j++) {
         cin >> input;
         if (input != "NULL") {
-            all_processes[0].p_events[i].e = input;
-            all_processes[0].all_events++;
+            all_processes[i].p_events[j].e = input;
+            all_processes[i].all_events++;
         }
-    }
-
-    cout << "Enter the events for the second process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> input;
-        if (input != "NULL") {
-            all_processes[1].p_events[i].e = input;
-            all_processes[1].all_events++;
-        }
-
-    }
-    cout << "Enter the events for the third process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> input;
-        if (input != "NULL") {
-            all_processes[2].p_events[i].e = input;
-            all_processes[2].all_events++;
-        }
-
+      }
     }
 
     // Showing the input
     // Showing P0
     cout << endl << "Your input is as follows: " << endl;
-    cout << "P0: ";
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-        cout << all_processes[0].p_events[i].e << " ";
+    for (int i = 0; i < N; i++) {
+      cout << "P" << i << ": ";
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        cout << all_processes[i].p_events[j].e << " ";
+      }
+      cout << endl;
     }
-    cout << endl << "P1: ";
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-        cout << all_processes[1].p_events[i].e << " ";
-    }
-    cout << endl << "P2: ";
-    for (int i = 0; i < all_processes[2].all_events; i++) {
-        cout << all_processes[2].p_events[i].e << " ";
-    }
-    cout << endl << endl;
-    cout << "The Solution is: " << endl;
+    cout << endl;
+    cout << "The solution is: " << endl;
+
+    //
     while (!all_done) {
+        // If the process still has events to assign LC values to
         if (all_processes[current_p].done == false) {
-            if (all_processes[current_p].p_events[all_processes[current_p].events_done].e[0] == 's') {
-                char_temp = &all_processes[current_p].p_events[all_processes[current_p].events_done].e[1];
+          current_event = all_processes[current_p].events_done;
+            // If the event is a send event
+            if (all_processes[current_p].p_events[current_event].e[0] == 's') {
+                char_temp = &all_processes[current_p].p_events[current_event].e[1];
                 send_arg = atoi(char_temp);
-                sendarray[send_arg] = all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value = all_processes[current_p].current_LCvalue + 1;
-                all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value = all_processes[current_p].current_LCvalue + 1;
-                all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value;
+                all_processes[current_p].p_events[current_event].LC_value = all_processes[current_p].current_LCvalue + 1;
+                all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[current_event].LC_value;
+                // Assigning send LC values to the send array
+                sendarray[send_arg] = all_processes[current_p].p_events[current_event].LC_value;
+                // If the process's events are all complete, then it's done
                 if (all_processes[current_p].events_done == all_processes[current_p].all_events - 1) {
                     all_processes[current_p].done = true;
                 }
@@ -109,12 +93,15 @@ int main() {
                     all_processes[current_p].events_done++;
                 }
             }
-            else if (all_processes[current_p].p_events[all_processes[current_p].events_done].e[0] == 'r') {
-                char_temp = &all_processes[current_p].p_events[all_processes[current_p].events_done].e[1];
+            // If the event is a receive event
+            else if (all_processes[current_p].p_events[current_event].e[0] == 'r') {
+                char_temp = &all_processes[current_p].p_events[current_event].e[1];
                 receive_arg = atoi(char_temp);
+                // If the send array has an entry for the event
+                // So r2 can only poceed if s2 has already been sent and recorded in send array
                 if (sendarray[receive_arg] < 100) {
-                    all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value = max(all_processes[current_p].current_LCvalue, sendarray[receive_arg]) + 1;
-                    all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value;
+                    all_processes[current_p].p_events[current_event].LC_value = max(all_processes[current_p].current_LCvalue, sendarray[receive_arg]) + 1;
+                    all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[current_event].LC_value;
                     if (all_processes[current_p].events_done == all_processes[current_p].all_events) {
                         all_processes[current_p].done = true;
                     }
@@ -129,9 +116,8 @@ int main() {
             }
             // the event is internal and will be assigned an LC value
             else {
-                all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value = all_processes[current_p].current_LCvalue + 1;
-
-                all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[all_processes[current_p].events_done].LC_value;
+                all_processes[current_p].p_events[current_event].LC_value = all_processes[current_p].current_LCvalue + 1;
+                all_processes[current_p].current_LCvalue = all_processes[current_p].p_events[current_event].LC_value;
                 if (all_processes[current_p].events_done == all_processes[current_p].all_events - 1) {
                     all_processes[current_p].done = true;
                 }
@@ -145,13 +131,13 @@ int main() {
         }
         if (all_processes[0].done == true && all_processes[1].done == true && all_processes[2].done == true) all_done = true;
     }
-    for(int i = 0; i < N; i++) {
-        event_count = 0;
-        while ( event_count < all_processes[i].all_events) {
-            cout << all_processes[i].p_events[event_count].LC_value << " ";
-            event_count++;
-        }
-        cout << endl;
+    // Printing the LC values for each event
+    for (int i = 0; i < N; i++) {
+      cout << "P" << i <<  ": ";
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        cout << all_processes[i].p_events[j].LC_value << " ";
+      }
+      cout << endl;
     }
     return 0;
 }
