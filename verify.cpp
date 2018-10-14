49d35bc08714d20ca0862407d9342a72769da991
Viet-Samle <viet.sam.n.le@gmail.com>
2018-10-14 16:03:52 -0700

    Update verify.cpp

--- verify.cpp
+++ verify.cpp
@@ -54,155 +54,74 @@ int main() {
     // Inputting LC-values
     // Inputting P0
     cout << "Enter the input for the logical clock" << endl;
-    cout << "Enter the LC values for the first process" << endl;
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < N; i++) {
+      cout << "Enter the LC values for process " << i << endl;
+      for (int j = 0; j < M; j++) {
         cin >> LC_input;
         if (LC_input != 0) {
-            all_processes[0].p_events[i].LC_value = LC_input;
-            all_processes[0].all_events++;
+            all_processes[i].p_events[j].LC_value = LC_input;
+            all_processes[i].all_events++;
         }
+      }
     }
-    // Inputting P1
-    cout << "Enter the LC values for the second process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> LC_input;
-        if (LC_input != 0) {
-            all_processes[1].p_events[i].LC_value = LC_input;
-            all_processes[1].all_events++;
-        }
-    }
-    // Inputting P2
-    cout << "Enter the LC for the third process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> LC_input;
-        if (LC_input != 0) {
-            all_processes[2].p_events[i].LC_value = LC_input;
-            all_processes[2].all_events++;
-        }
-    }
-
     // Showing LC value input
-    cout << endl << "Your input is as follows: " << endl;
-    cout << "P0: ";
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-        cout << all_processes[0].p_events[i].LC_value << " ";
+    for (int i = 0; i < N; i++) {
+      cout << "P" << i << ": ";
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        cout << all_processes[i].p_events[j].LC_value << " ";
+      }
+      cout << endl;
     }
-    cout << endl << "P1: ";
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-        cout << all_processes[1].p_events[i].LC_value << " ";
-    }
-    cout << endl << "P2: ";
-    for (int i = 0; i < all_processes[2].all_events; i++) {
-        cout << all_processes[2].p_events[i].LC_value << " ";
-    }
-    cout << endl << endl;
-
+    cout << endl;
 
     // Inputting events to Verify
-    // Inputting events for P0
-    cout << "Enter the events for the first process" << endl;
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < N; i++) {
+      cout << "Enter the events for process " << i << endl;
+      for (int j = 0; j < M; j++) {
         cin >> event_input;
         if (event_input != "NULL") {
-            all_processes[0].p_events[i].given_event = event_input;
+            all_processes[i].p_events[j].given_event = event_input;
         }
-    }
-    // Inputting events for P1
-    cout << "Enter the events for the second process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> event_input;
-        if (event_input != "NULL") {
-            all_processes[1].p_events[i].given_event = event_input;
-        }
-    }
-    // Inputting events for P2
-    cout << "Enter the events for the third process" << endl;
-    for (int i = 0; i < M; i++) {
-        cin >> event_input;
-        if (event_input != "NULL") {
-            all_processes[2].p_events[i].given_event = event_input;
-        }
-    }
-
-    // Showing event input
-    cout << endl << "Your input is as follows: " << endl;
-    cout << "P0: ";
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-        cout << all_processes[0].p_events[i].given_event << " ";
-    }
-    cout << endl << "P1: ";
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-        cout << all_processes[1].p_events[i].given_event << " ";
-    }
-    cout << endl << "P2: ";
-    for (int i = 0; i < all_processes[2].all_events; i++) {
-        cout << all_processes[2].p_events[i].given_event << " ";
-    }
-    cout << endl << endl;
-
-    // Assigning labels (internal/send/receive) for each LC_value
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-      if (all_processes[0].p_events[i].LC_value != all_processes[0].current_LCvalue + 1) {
-        all_processes[0].p_events[i].label = 'r';
-        rcv_array[rcv_count++] = all_processes[0].p_events[i].LC_value;
       }
-      all_processes[0].current_LCvalue = all_processes[0].p_events[i].LC_value;
     }
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-      if (all_processes[1].p_events[i].LC_value != all_processes[1].current_LCvalue + 1) {
-        all_processes[1].p_events[i].label = 'r';
-        rcv_array[rcv_count++] = all_processes[1].p_events[i].LC_value;
+    // Showing events to verify
+    for (int i = 0; i < N; i++) {
+      cout << "P" << i << ": ";
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        cout << all_processes[i].p_events[j].given_event << " ";
       }
-      all_processes[1].current_LCvalue = all_processes[1].p_events[i].LC_value;
-    }
-    for (int i = 0; i < all_processes[2].all_events; i++) {
-      if (all_processes[2].p_events[i].LC_value != all_processes[2].current_LCvalue + 1) {
-        all_processes[2].p_events[i].label = 'r';
-        rcv_array[rcv_count++] = all_processes[2].p_events[i].LC_value;
-      }
-      all_processes[2].current_LCvalue = all_processes[2].p_events[i].LC_value;
-    }
-    for (int i = 0; i < 10; i++) {
-      cout << rcv_array[i] << " ";
+      cout << endl;
     }
     cout << endl;
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-      for (int j = 0; j < rcv_count; j++) {
-        if (all_processes[0].p_events[i].LC_value == rcv_array[j] - 1) {
-          all_processes[0].p_events[i].label = 's';
-          rcv_array[j] = 100;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        if (all_processes[i].p_events[j].LC_value != all_processes[i].current_LCvalue + 1) {
+          all_processes[i].p_events[i].label = 'r';
+          rcv_array[rcv_count++] = all_processes[i].p_events[j].LC_value;
+        }
+        all_processes[i].current_LCvalue = all_processes[i].p_events[j].LC_value;
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        for (int k = 0; k < rcv_count; k++) {
+          if (all_processes[i].p_events[j].LC_value == rcv_array[k] - 1) {
+            all_processes[i].p_events[j].label = 's';
+            rcv_array[k] = 100;
+          }
         }
       }
     }
 
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-      for (int j = 0; j < rcv_count; j++) {
-        if (all_processes[1].p_events[i].LC_value == rcv_array[j] - 1) {
-          all_processes[1].p_events[i].label = 's';
-          rcv_array[j] = 100;
-        }
+    cout << "Label for given events are" << endl;
+    for (int i = 0; i < N; i++) {
+      cout << "P" << i << ": ";
+      for (int j = 0; j < all_processes[i].all_events; j++) {
+        cout << all_processes[i].p_events[j].label << " ";
       }
-    }
-
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-      for (int j = 0; j < rcv_count; j++) {
-        if (all_processes[2].p_events[i].LC_value == rcv_array[j] - 1) {
-          all_processes[2].p_events[i].label = 's';
-          rcv_array[j] = 100;
-        }
-      }
-    }
-
-    for (int i = 0; i < all_processes[0].all_events; i++) {
-      cout << all_processes[0].p_events[i].label << " ";
-    }
-    cout << endl;
-    for (int i = 0; i < all_processes[1].all_events; i++) {
-      cout << all_processes[1].p_events[i].label << " ";
-    }
-    cout << endl;
-    for (int i = 0; i < all_processes[2].all_events; i++) {
-      cout << all_processes[2].p_events[i].label << " ";
+      cout << endl;
     }
     cout << endl;
 
@@ -216,26 +135,12 @@ int main() {
         else if(all_processes[i].p_events[j].label == 'r') {
           tmp = &all_processes[i].p_events[j].given_event[1];
           arg = atoi(tmp);
-          cout << arg << " | "<< all_processes[i].p_events[j].LC_value << endl;
           rcv_array[arg] = all_processes[i].p_events[j].LC_value;
         }
       }
     }
-    cout << "Receive Array: ";
-    for (int i = 0; i < 10; i++) {
-      cout << rcv_array[i] << " ";
-    }
-    cout << endl;
-    cout << "Send Array: ";
-    for (int i = 0; i < 10; i++) {
-      cout << send_array[i] << " ";
-    }
-    cout << endl;
-
 
     for (int i = 1; i < rcv_count + 1; i++) {
-      cout << rcv_array[i] << " | " << send_array[i] << endl;
-      cout << correct << endl;
       if (!(rcv_array[i] > send_array[i])) {
         correct = false;
         break;
@@ -247,4 +152,5 @@ int main() {
 
     return 0;
 }
+
 
